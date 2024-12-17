933042674ce61092c7e221a63fa8371a115c975f
Galen Borneo Wibowo <borneo.wibowo@gmail.com>
2024-12-17 21:33:48 +0700

    Update add search catagory on search

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
   <body>
     <!-- Navbar -->
     <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div class="max-w-screen-full flex flex-wrap items-center justify-between mx-auto p-4">
+      <div class="max-w-screen-full flex flex-wrap items-center justify-between mx-auto py-4 px-10">
         <!-- Kiri -->
         <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img
@@ -30,34 +30,27 @@
         </a>
 
         <!-- Tengah -->
-        <div class="flex-grow flex justify-center md:order-1">
-          <div class="relative w-1/2 mx-20 hidden md:block">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg
-                class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-              <span class="sr-only">Search icon</span>
+        <form class="max-w-lg mx-auto hidden md:block">
+            <div class="flex">
+                <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+                <button id="dropdownCategory-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">
+                        All categories 
+                    <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                </button>
+                <div class="relative w-full">
+                    <input type="search" id="search-dropdown" class="block p-2.5 w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Cari judul buku disini..."/>
+                    <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                        <span class="sr-only">Search</span>
+                    </button>
+                </div>
             </div>
-            <input
-              type="text"
-              id="search-navbar"
-              class="w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Cari judul buku kamu disini !"
-            />
-          </div>
-        </div>
+        </form>
+        
 
         <!-- Kanan -->
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -88,7 +81,7 @@
         </li>
         <li>
           <a
-            href="#"
+            href="{{ route('home') }}"
             class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >Home</a
           >
@@ -109,16 +102,31 @@
         </li>
       </ul>
       <div class="py-2">
-        <a
-          href="#"
-          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-          >Logout</a
-        >
-      </div>
+        <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+            Logout
+        </a>
+    </div>
+    </div>
+    <!-- Dropdown category -->
+    <div id="dropdownCategory" class="absolute z-10 top-20 right-1/2 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-56 dark:bg-gray-700">
+        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+        <li>
+            <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Category 1</button>
+        </li>
+        <li>
+            <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Category 2</button>
+        </li>
+        <li>
+            <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Category 3</button>
+        </li>
+        <li>
+            <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Category 4</button>
+        </li>
+        </ul>
     </div>
 
     <div class="p-3 mt-28 flex justify-center items-center flex-col gap-4 md:grid md:grid-cols-4 md:gap-4 md:justify-center md:justify-items-center">
-      <div class="w-96  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div class="w-96 max-h-[600px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <img
             class="rounded-t-lg max-h-60 w-full object-cover object-center"
@@ -135,13 +143,13 @@
             </h5>
           </a>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Penulis Buku
+            deskripsi buku
           </p>
           <a
             href="#"
             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Read more
+            Lihat Buku
             <svg
               class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
               aria-hidden="true"
@@ -164,6 +172,7 @@
 
     <!-- SCRIPT -->
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+    {{-- Script user --}}
     <script>
       function toggleDrawer() {
           const drawer = document.getElementById('dropdownDivider');
@@ -178,5 +187,12 @@
           }
       }
   </script>
+  {{-- Script category --}}\
+  <script>
+    document.getElementById('dropdownCategory-button').addEventListener('click', function() {
+        var dropdown = document.getElementById('dropdownCategory');
+        dropdown.classList.toggle('hidden');
+    });
+</script>
   </body>
 </html>
